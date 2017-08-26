3348015c72df3e59ec41af5fe366d072a50d295e
wkm23@126.com <wkm23@126.com>
2017-08-27 11:09:06 +1200

    bash script

---
+++ jenkins-build.sh
@@ -0,0 +1,5 @@
+echo 'Start Copying bash script...'
+cp -f ./start.sh /usr/share/kyle-applications/ofbiz/
+cp -f ./build.sh /usr/share/kyle-applications/ofbiz/
+echo 'String Running bash script...'
+sh /usr/share/kyle-applications/ofbiz/build.sh
